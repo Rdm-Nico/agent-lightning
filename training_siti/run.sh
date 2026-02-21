6384293e08e4e245547291539b45023416bab0f8
Nicolò Rossi <nicorossi2810@gmail.com>
2026-02-21 16:02:30 +0000

    adding script

---
+++ training_siti/run.sh
@@ -0,0 +1,12 @@
+# script for run the ft 
+cd ..
+ulimit -c 0 && \
+python training_siti/train_siti_agent.py \
+    --train-file training_siti/data/train.parquet \
+    --val-file training_siti/data/test.parquet \
+    --lora \
+    --lora-rank 16 \
+    --trajectory-level \
+    --n-runners 10 \
+    --agent-match chat \
+    --start-embedding
